a4fc4175a5f956cde656c2a0e682b5cca014a54c
Tim McIver <tim@timmciver.com>
2019-10-19 22:11:00 -0400

    More sophisticated CLI.

--- app/CLI/Main.hs
+++ app/CLI/Main.hs
@@ -1,18 +1,29 @@
 
 data Command = Create | Show | Quit deriving (Show, Eq)
 
+toCommand :: String
+          -> Maybe Command
+toCommand "q" = Just Quit
+toCommand "c" = Just Create
+toCommand "s" = Just Show
+toCommand _ = Nothing
+
 getCommand :: IO Command
 getCommand = do
-  putStrLn "Enter a command: q (quit)"
-  _ <- getLine
-  return Quit
+  putStrLn "Enter a command: c (create), q (quit)"
+  inStr <- getLine
+  case toCommand inStr of
+    Just cmd -> return cmd
+    Nothing -> putStrLn "Invalid Command." >> getCommand
 
-doCommand :: IO ()
-doCommand = main
+doCommand :: Command
+          -> IO ()
+doCommand _ = main
 
 main :: IO ()
 main = do
   cmd <- getCommand
+  putStrLn ("You entered: " ++ (show cmd))
   if cmd == Quit then
     return ()
-    else doCommand
+    else doCommand cmd

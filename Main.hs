665b9fc5412a3c75168334234b7ba8e22b46b2dc
Tim McIver <tim@timmciver.com>
2019-10-19 20:43:28 -0400

    Give a default value to the URL path parameter.

--- Main.hs
+++ Main.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Web.Scotty
+import Data.Text as T
+import Data.Text.Lazy (toStrict)
 
 main :: IO ()
 main = scotty 3000 $ do
   get "/:word" $ do
-    beam <- param "word"
+    word <- param "word"
+    let beam = if T.null (toStrict word) then "beam" else word
     html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]

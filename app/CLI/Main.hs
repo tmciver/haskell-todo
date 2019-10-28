c29947d66c3bae73e29f7ae5e893e84525dc8f3e
Tim McIver <tim@timmciver.com>
2019-10-27 23:32:24 -0400

    Implement CLI using pipes. There's still a problem with flushing stdout; Ctl-D doesn't work quite right. It takes repeated Ctl-D's after running any command.

--- app/CLI/Main.hs
+++ app/CLI/Main.hs
@@ -12,16 +12,17 @@ import qualified Pipes.Prelude as P
 
 commandProducer :: Producer Command IO ()
 commandProducer = do
-  lift $ putStrLn "Enter a command: c (create), s (show); (Ctl-D to quit)"
+  lift $ putStrLn "Enter a command: c (create), s (show); (Ctl-D to quit)" >> hFlush stdout
   for P.stdinLn (\str -> do
-                    case toCommand str of
-                      Just cmd -> yield cmd
-                      Nothing -> (lift $ putStrLn "Invalid Command.") >> (lift $ hFlush stdout) >> commandProducer)
+                    _ <- case toCommand str of
+                           Just cmd -> yield cmd
+                           Nothing -> lift $ putStrLn "Invalid Command." >> hFlush stdout
+                    commandProducer)
 
-printerConsumer :: Show a
-                => a
+commandConsumer :: TodoRepository
+                -> Command
                 -> Effect IO ()
-printerConsumer = lift . putStrLn . show
+commandConsumer repo cmd = lift $ doCommand repo cmd
 
 -- end Pipes stuff
 
@@ -50,27 +51,15 @@ toCommand "c" = Just Create
 toCommand "s" = Just Show
 toCommand _ = Nothing
 
-getCommand :: IO Command
-getCommand = do
-  putStrLn "Enter a command: c (create), s (show), q (quit)"
-  inStr <- getLine
-  case toCommand inStr of
-    Just cmd -> return cmd
-    Nothing -> putStrLn "Invalid Command." >> getCommand
-
 doCommand :: TodoRepository
           -> Command
           -> IO ()
 doCommand repo Show = do
   todos <- getAll repo
   (putStrLn . show) todos
-  loop repo
-doCommand repo Create = createTodo >>= (save repo) >> loop repo
-
-loop :: TodoRepository
-     -> IO ()
-loop repo = getCommand >>= doCommand repo
+doCommand repo Create = createTodo >>= (save repo)
 
 main :: IO ()
---main = inMemoryTodoRepo >>= loop
-main = runEffect $ for commandProducer printerConsumer
+main = do
+  repo <- inMemoryTodoRepo
+  runEffect $ for commandProducer (commandConsumer repo)
